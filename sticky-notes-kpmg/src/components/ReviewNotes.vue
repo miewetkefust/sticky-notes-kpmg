1a2e89abf4c000666ee46c0c39f3bfb50d124929
Mie Wetke Fust <wetkefust@gmail.com>
2021-09-14 13:58:38 +0200

    Setup of express js

--- sticky-notes-kpmg/src/components/ReviewNotes.vue
+++ sticky-notes-kpmg/src/components/ReviewNotes.vue
@@ -1,9 +1,34 @@
 <template>
-    <h1>Review Notes</h1>
+<div>
+    <div id="titleBar">
+        <h1>Review notes</h1>
+        <v-app>
+            <v-btn v-on:click="newNote"
+                color="blue"
+                margin="5px"
+            >New</v-btn>
+        </v-app>
+    </div>
+    
+</div>
+    
 </template>
 
 <script>
 export default {
-    name: "ReviewNotes"
+    name: "ReviewNotes",
+    methods: {
+        newNote: function() {
+            //Todo
+            console.log("CLicked")
+        }
+    }
 }
 </script>
+
+<style scoped>
+    #titleBar {
+        display: flex;
+        flex-direction: row;
+    }
+</style>
