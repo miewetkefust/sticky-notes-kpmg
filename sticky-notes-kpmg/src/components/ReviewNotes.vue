7ab207d0e4fd2368823a145fc5e4f04da44dbb22
Mie Wetke Fust <wetkefust@gmail.com>
2021-09-14 12:37:40 +0200

    Skeleton for first component

---
+++ sticky-notes-kpmg/src/components/ReviewNotes.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Review Notes</h1>
+</template>
+
+<script>
+export default {
+    name: "ReviewNotes"
+}
+</script>
--- sticky-notes-kpmg/src/main.js
+++ sticky-notes-kpmg/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import ReviewNotes from './components/ReviewNotes'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/review-notes', component: ReviewNotes}
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
