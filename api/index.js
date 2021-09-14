3473fa7aedd4a0a327dd503ba40e22504c8079e5
Mie Wetke Fust <wetkefust@gmail.com>
2021-09-14 15:46:32 +0200

    API functionality

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
 import express from 'express'
+import fs from 'fs'
 
 const app = express()
 const port = 3000
 
+const rawdataUsers = fs.readFileSync('dummy-data-users.json')
+const rawdataNotes = fs.readFileSync('dummy-data-reviewnotes.json')
+const users = JSON.parse(rawdataUsers)
+const reviewNotes = JSON.parse(rawdataNotes)
+
+const fullNotes = reviewNotes.map(note => {
+    const ass = note.assignees
+    const detailedAss = ass.map(a => users.find(u => u.id === a.$oid))
+    note.assignees = detailedAss
+    return note
+})
+
 app.get('/', (req,res) => {
-    res.send('Hello World')
+    res.send(fullNotes)
 })
 
 app.listen(port, () => {
