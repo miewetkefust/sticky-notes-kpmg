1a1b634a2cfc8678dfd4097dfe639016d74c57a8
Mie Wetke Fust <wetkefust@gmail.com>
2021-09-15 11:11:44 +0200

    First few parameters of list view done

--- sticky-notes-kpmg/src/components/NotesList.vue
+++ sticky-notes-kpmg/src/components/NotesList.vue
@@ -1,7 +1,40 @@
 <template>
-    <div>
-        <p v-for="(note, index) in notes" :key="index"></p>
-    </div>
+<div>
+    <v-container 
+        class="d-flex align-center"
+        v-for="(note, index) in notes" :key="index">
+
+        <v-row>
+             <!--checkbox and title-->
+             <v-col>
+                 <v-checkbox 
+                :label="note.title"/>
+             </v-col>
+            
+
+            <!--type-->
+            <v-col>
+                <v-chip
+                class="ma-2"
+                color="secondary"
+                label>
+                {{note.type}}
+                </v-chip>
+            </v-col>
+            
+            <!--status-->
+            <v-col>
+                <v-chip
+                :color="statusColor(note.status)"
+                class="ma-2"
+                label>
+                {{note.status}}
+                </v-chip>
+            </v-col>
+        </v-row>
+       
+    </v-container>
+</div>
 </template>
 
 <script>
@@ -16,6 +49,22 @@ export default {
         const response = await fetch('http://localhost:3000')
         const json = await response.json()
         this.notes = json
+    },
+    methods: {
+        statusColor: function(status){
+            switch(status.toLowerCase()){
+                case "in progress":
+                    return "primary"
+                case "pending documentation":
+                    return "orange"
+                case "not started":
+                    return "secondary"
+                case "closed":
+                    return "red"
+                default:
+                    return ""
+            }
+        }
     }
 }
 </script>
