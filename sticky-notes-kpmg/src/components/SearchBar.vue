82992b7014507c89a2e7c1ef13fe74faff683361
Mie Wetke Fust <wetkefust@gmail.com>
2021-09-15 10:41:12 +0200

    Skeleton for search bar

--- sticky-notes-kpmg/src/components/SearchBar.vue
+++ sticky-notes-kpmg/src/components/SearchBar.vue
@@ -1,5 +1,63 @@
 <template>
-    <div></div>
+    <v-container>
+        <v-row>
+            <v-text-field
+            placeholder="Quick Search"
+            outlined/>
+        
+                <v-col>
+                    <p>Types</p>
+                    <v-btn-toggle mandatory>
+                        <v-btn>All</v-btn>
+                        <v-btn>Tasks</v-btn>
+                        <v-btn>Notes</v-btn>
+                    </v-btn-toggle>
+                </v-col>
+                
+                <v-col>
+                    <p>Priority</p>
+                    <v-btn-toggle mandatory>
+                        <v-btn>All</v-btn>
+                        <v-btn>Low</v-btn>
+                        <v-btn>Medium</v-btn>
+                        <v-btn>High</v-btn>
+                    </v-btn-toggle>
+                </v-col>
+                
+                <v-col>
+                    <p>Reporter</p>
+                    <v-chip
+                        color="primary"
+                        close>
+                        Bil Gates
+                    </v-chip>
+                </v-col>
+
+                <v-col>
+                    <p>Assignees</p>
+                    <v-chip
+                        color="primary"
+                        close>
+                        Bil Gates
+                    </v-chip>
+                </v-col>
+
+                <v-col>
+                    <p>Section</p>
+                    <v-chip
+                        color="primary"
+                        close>
+                        Bil Gates
+                    </v-chip>
+                </v-col>
+
+                <v-col>
+                    <p>Date</p>
+                    <input type="date"/>
+                </v-col>
+        </v-row>
+        <a href="http://localhost:8080/#/review-notes">Clear applied filters</a>
+    </v-container>
 </template>
 
 <script>
